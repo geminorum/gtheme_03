0d05eaff30004190fe7bd7f937bc1e4d468a182f
Nasser Rafie <contact@geminorum.ir>
2014-10-29 18:50:17 +0330

    bootstrap and assets

---
+++ assets/css.php
@@ -0,0 +1,20 @@
+<?php
+
+// SAMPLE
+
+echo '@charset "UTF-8";';
+
+include('../../gtheme_03/libs/bootstrap/css/bootstrap.min.css');
+include('../../gtheme_03/libs/bootstrap-rtl/css/bootstrap-rtl.min.css');
+include('../../gtheme_03/libs/genericons/genericons/genericons.css');
+
+include('../../gtheme_03/css/bootstrap-rtl_01.css');
+include('../../gtheme_03/css/defaults-rtl_01.css');
+
+include('fonts.css');
+include('defaults.css');
+include('theme.css');
+include('queries.css');
+include('colors.css');
+
+?>
---
+++ assets/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include '../../gtheme_03/assets/functions.php';
+include '../../gtheme_03/assets/headers-css.php';
+
+ob_start( 'minify_css' );
+	include 'css.php';
+ob_end_flush();
+
+?>
