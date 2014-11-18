194ed604628702b1669f9f28953ad795731da2b8
Nasser Rafie <contact@geminorum.ir>
2014-11-18 20:50:34 +0330

    more fallbacks

--- inc/fallbacks.php
+++ inc/fallbacks.php
@@ -2,3 +2,16 @@
 
 // gMeta & gEditorialMeta
 if ( ! function_exists( 'gmeta' ) ) : function gmeta(){} endif;
+if ( ! function_exists( 'gmeta_label' ) ) : function gmeta_label() {} endif;
+if ( ! function_exists( 'gmeta_lead' ) ) : function gmeta_lead() {} endif;
+if ( ! function_exists( 'gmeta_author' ) ) : 
+	function gmeta_author( $b = '', $a = '', $f = false, $args = array() ) {
+		$author = get_the_author();
+		if ( empty( $author ) )
+			return ( isset( $args['def'] ) ? $args['def'] : false );
+		$html = $b.( $f ? $f( $author ) : $author ).$a;
+		if ( isset( $args['echo'] ) && ! $args['echo'] )
+			return $html;
+		echo $html;
+	}
+endif;
