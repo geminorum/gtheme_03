06f49f949d59d5d98f913272da82fdf27a2d31da
Nasser Rafie <contact@geminorum.ir>
2014-11-21 06:27:39 +0330

    children and siblings shortcodes

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -5,7 +5,7 @@ class gThemeShortCodes extends gThemeModuleCore
 
 	function setup_actions( $args = array() )
 	{
-		add_action( 'init', array( & $this, 'init' ), 12 );
+		add_action( 'init', array( & $this, 'init' ), 14 );
 	}
 	
 	public function init()
@@ -14,6 +14,8 @@ class gThemeShortCodes extends gThemeModuleCore
 			'theme-image' => 'shortcode_theme_image',
 			'panel-group' => 'shortcode_panel_group',
 			'panel' => 'shortcode_panel',
+			'children' => 'shortcode_children',
+			'siblings' => 'shortcode_siblings',
 		);
 	
 		foreach ( $shortcodes as $shortcode => $method ) {
@@ -114,5 +116,139 @@ class gThemeShortCodes extends gThemeModuleCore
 			), $html );
 		
 		return $html;
+	}
+	
+	function shortcode_children( $atts, $content = null, $tag = '' ) 
+	{
+		$args = shortcode_atts( array(
+			'id' => get_queried_object_id(),
+			'type' => 'page',
+			'excerpt' => true,
+		), $atts, $tag );
+
+		if ( ! $args['id'] )
+			return $content;
+		
+		if ( ! is_singular( $args['type'] ) )
+			return $content;
+			
+		$children = wp_list_pages( array( 
+			'child_of' => $args['id'],
+			'post_type' => $args['type'],
+			'excerpt' => $args['excerpt'],
+			'echo' => false,
+			'depth' => 1,
+			'title_li' => '',
+			'sort_column' => 'menu_order, post_title',
+			'walker' => new gTheme_Walker_Page(),
+		) );
+		
+		if ( ! $children )
+			return $content;
+		
+		return '<div class="list-group children">'.$children.'</div>';
+	}
+	
+	function shortcode_siblings( $atts, $content = null, $tag = '' ) 
+	{
+		$args = shortcode_atts( array(
+			'parent' => null,
+			'type' => 'page',
+			'excerpt' => true,
+		), $atts, $tag );
+
+		if ( ! is_singular( $args['type'] ) )
+			return $content;
+		
+		if ( is_null( $args['parent'] ) ) {
+			$object = get_queried_object();
+			if ( $object && isset( $object->post_parent ) )
+				$args['parent'] = $object->post_parent;
+		}
+		
+		if ( ! $args['parent'] )
+			return $content;
+			
+		$siblings = wp_list_pages( array( 
+			'child_of' => $args['parent'],
+			'post_type' => $args['type'],
+			'excerpt' => $args['excerpt'],
+			'echo' => false,
+			'depth' => 1,
+			'title_li' => '',
+			'sort_column' => 'menu_order, post_title',
+			'walker' => new gTheme_Walker_Page(),
+		) );
+		
+		if ( ! $siblings )
+			return $content;
+		
+		return '<div class="list-group siblings">'.$siblings.'</div>';
+	}	
+}
+
+class gTheme_Walker_Page extends Walker_Page 
+{
+	public function start_el( & $output, $page, $depth = 0, $args = array(), $current_page = 0 ) 
+	{
+		$css_class = array( 'list-group-item', 'page-item-'.$page->ID );
+
+		if ( isset( $args['pages_with_children'][$page->ID] ) )
+			$css_class[] = 'page_item_has_children';
+
+		if ( ! empty( $current_page ) ) {
+			$_current_page = get_post( $current_page );
+			if ( $_current_page && in_array( $page->ID, $_current_page->ancestors ) ) {
+				$css_class[] = 'current_page_ancestor';
+			}
+			if ( $page->ID == $current_page ) {
+				$css_class[] = 'active';
+				$css_class[] = 'current_page_item';
+			} elseif ( $_current_page && $page->ID == $_current_page->post_parent ) {
+				$css_class[] = 'current_page_parent';
+			}
+		} elseif ( $page->ID == get_option('page_for_posts') ) {
+			$css_class[] = 'current_page_parent';
+		}
+
+		$css_classes = implode( ' ', apply_filters( 'page_css_class', $css_class, $page, $depth, $args, $current_page ) );
+
+		if ( '' === $page->post_title )
+			$page->post_title = sprintf( __( '#%d (no title)' ), $page->ID );
+
+		if ( isset( $args['excerpt'] ) && $args['excerpt'] && ! empty( $page->post_excerpt ) ) {
+			$output .= sprintf(
+				'<a class="%s" href="%s"><h4 class="list-group-item-heading">%s</h4><p class="list-group-item-text">%s</p></a>',
+				$css_classes,
+				get_permalink( $page->ID ),
+				apply_filters( 'the_title', $page->post_title, $page->ID ),
+				$page->post_excerpt
+			);
+		} else {
+			$output .= sprintf(
+				'<a class="%s" href="%s">%s</a>',
+				$css_classes,
+				get_permalink( $page->ID ),
+				apply_filters( 'the_title', $page->post_title, $page->ID )
+			);
+		}
+		
+		/**
+		if ( ! empty( $args['show_date'] ) ) {
+			if ( 'modified' == $args['show_date'] ) {
+				$time = $page->post_modified;
+			} else {
+				$time = $page->post_date;
+			}
+
+			$date_format = empty( $args['date_format'] ) ? '' : $args['date_format'];
+			$output .= " " . mysql2date( $date_format, $time );
+		}
+		**/
+	}
+	
+	public function end_el( &$output, $page, $depth = 0, $args = array() ) 
+	{
+		$output .= "\n";
 	}
 }
