2e29d8a619aac9497b1c1a0333b6769443a0903a
Nasser Rafie <contact@geminorum.ir>
2014-12-02 01:52:22 +0330

    adding wp core id filter

--- inc/menu.php
+++ inc/menu.php
@@ -118,8 +118,8 @@ class gThemeMenu extends gThemeModuleCore
 		
 		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 		return gThemeUtilities::html( 'li', array(
-			'id' => 'menu-item-'.$item->ID,
-			'class' => apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, array() ),
+			'id' => apply_filters( 'nav_menu_item_id', 'menu-item-'.$item->ID, $item, array(), 0 ),
+			'class' => apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, array(), 0 ),
 		), $html );
 	}
 }
