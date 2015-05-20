28e317ffa21113620ac003d208847a9d5ce51246
Nasser Rafie <contact@geminorum.ir>
2015-05-20 17:10:38 +0430

    TTL for a year!

--- assets/headers-css.php
+++ assets/headers-css.php
@@ -14,7 +14,7 @@ if ( isset ( $_GET['debug'] ) && 'debug' == $_GET['debug'] ) {
 
 	ini_set( 'zlib.output_compression', 4096 );
 
-	$offset = 60 * 60 * 24; // for a day
+	$offset = 60 * 60 * 24 * 365; // for a day * 365
 	header( 'Cache-Control: max-age='.$offset.', must-revalidate' );
 	header( 'Expires: '.gmdate( 'D, d M Y H:i:s', time() + $offset ).' GMT' );	
 
