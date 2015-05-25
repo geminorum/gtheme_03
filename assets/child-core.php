bf79e11e80dc7da29ed0ef272ea31e816e3c8b9c
Nasser Rafie <contact@geminorum.ir>
2015-05-25 23:41:37 +0430

    child core class
    + utility mixins
    + asset include function
    + comments for base.php

---
+++ assets/child-core.php
@@ -0,0 +1,13 @@
+<?php defined( 'ABSPATH' ) or die( 'Restricted access' );
+
+if ( ! class_exists( 'gThemeChildCore' ) ) {
+class gThemeChildCore
+{
+	public function __construct()
+	{
+		$this->setup_actions();
+	}
+	
+	public function setup_actions() {}
+}
+}
