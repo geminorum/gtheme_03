519c52313a7facb6ebf975c25c85848ef3a996d9
Nasser Rafie <contact@geminorum.ir>
2015-07-31 21:29:01 +0430

    better row html

--- row.php
+++ row.php
@@ -1,1 +1,1 @@
-<a href="<?php the_permalink(); ?>"><?php get_the_title() ? the_title() : the_ID(); ?></a>
+<a class="permalink" title="<?php gThemeContent::title_attr(); ?>" href="<?php the_permalink(); ?>"><?php get_the_title() ? the_title() : the_ID(); ?></a>
