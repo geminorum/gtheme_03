1818e9aa2e27f7485f89fd6c1d2c03434d368518
Nasser Rafie <contact@geminorum.ir>
2015-08-24 05:00:13 +0430

    :wrench:

--- content.php
+++ content.php
@@ -1,12 +1,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry-wrap content-index clearfix' ); ?>><?php
 
-		gThemeImage::image( array( 'tag' => 'single' ) );
-		gThemeContent::header( array( 'context' => 'index', ) );
+	gThemeImage::image( array( 'tag' => 'single' ) );
+	gThemeContent::header( array( 'context' => 'index', ) );
 
-		if ( is_singular() ) {
-			gThemeContent::content();
-		} else {
-			gThemeContent::excerpt();
-		}
+	if ( is_singular() ) {
+		gThemeContent::content();
+	} else {
+		gThemeContent::excerpt();
+	}
 
 ?></article>
--- inc/fallbacks.php
+++ inc/fallbacks.php
@@ -1,10 +1,10 @@
 <?php defined( 'ABSPATH' ) or die( 'Restricted access' );
 
 // gMeta & gEditorialMeta
-if ( ! function_exists( 'gmeta' ) ) : function gmeta(){} endif;
-if ( ! function_exists( 'gmeta_label' ) ) : function gmeta_label() {} endif;
+if ( ! function_exists( 'gmeta' ) ) : function gmeta(){} endif; // DEPRECATED: use gThemeEditorial::meta()
+if ( ! function_exists( 'gmeta_label' ) ) : function gmeta_label() {} endif; // DEPRECATED: use gThemeEditorial::label()
 if ( ! function_exists( 'gmeta_lead' ) ) : function gmeta_lead() {} endif;
-if ( ! function_exists( 'gmeta_author' ) ) : 
+if ( ! function_exists( 'gmeta_author' ) ) :
 	function gmeta_author( $b = '', $a = '', $f = false, $args = array() ) {
 		$author = get_the_author();
 		if ( empty( $author ) )
