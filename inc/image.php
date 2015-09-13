194a80028901636936817878bc0c30c3eca3bca4
Nasser Rafie <contact@geminorum.ir>
2015-09-14 04:07:56 +0430

    register title on additional sizes

--- inc/image.php
+++ inc/image.php
@@ -40,10 +40,11 @@ class gThemeImage extends gThemeModuleCore
 
 	public function init()
 	{
-		foreach ( gtheme_get_info( 'images', array() ) as $name => $size )
-			self::addImageSize( $name, $size['w'], $size['h'], $size['c'], $size['p'] );
+		foreach ( gThemeOptions::info( 'images', array() ) as $name => $size )
+			self::registerImageSize( $name, $size );
 	}
 
+	// FIXME: DEPRECATED
 	// this must be wp core feature!
 	// core duplication with post_type : add_image_size()
 	public static function addImageSize( $name, $width = 0, $height = 0, $crop = FALSE, $post_type = array( 'post' ) )
@@ -55,6 +56,27 @@ class gThemeImage extends gThemeModuleCore
 			'height'    => absint( $height ),
 			'crop'      => $crop,
 			'post_type' => $post_type,
+		);
+	}
+
+	public static function registerImageSize( $name, $atts = array() )
+	{
+		global $_wp_additional_image_sizes;
+
+		$args = self::atts( array(
+			'n' => __( 'Undefined Image Size', GTHEME_TEXTDOMAIN ),
+			'w' => 0,
+			'h' => 0,
+			'c' => 0,
+			'p' => array( 'post' ),
+		), $atts );
+
+		$_wp_additional_image_sizes[$name] = array(
+			'width'     => absint( $args['w'] ),
+			'height'    => absint( $args['h'] ),
+			'crop'      => $args['c'],
+			'post_type' => $args['p'],
+			'title'     => $args['n'],
 		);
 	}
 
