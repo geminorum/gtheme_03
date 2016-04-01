02cee9cdb32c6b130950ddd42b83bac930bd10d8
Nasser Rafie <contact@geminorum.ir>
2016-04-01 20:45:01 +0430

    script debug constant is always defined

--- inc/theme.php
+++ inc/theme.php
@@ -123,7 +123,7 @@ class gThemeTheme extends gThemeModuleCore
 
 	public function wp_enqueue_scripts()
 	{
-		$suffix = ( ( ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) || gThemeUtilities::isDev() ) ? '' : '.min' );
+		$suffix = SCRIPT_DEBUG ? '' : '.min';
 
 		wp_enqueue_script( 'gtheme-all', GTHEME_URL."/js/script.all$suffix.js", array( 'jquery' ), GTHEME_VERSION, TRUE );
 
