a70bf459b4b06b993a4a890fb29465c703354cd9
Nasser Rafie <contact@geminorum.ir>
2016-05-19 00:55:38 +0430

    beautify and eols

--- js/script.gallery.js
+++ js/script.gallery.js
@@ -1,31 +1,33 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
 	var lastDirection;
 
 	$('.gallery').imagesLoaded(function() {
 		$('.gallery-spinner').fadeOut(500, function() {
-			$('.gallery').animate({'opacity' : '1'}, 500);
+			$('.gallery').animate({
+				'opacity': '1'
+			}, 500);
 		});
 	});
 
-	$('.gallery-img').bind('mouseenter', function(e){
+	$('.gallery-img').bind('mouseenter', function(e) {
 
 		var img = this;
 		lastDirection = getDir($(this), e);
 
 		$(img).addClass(lastDirection);
 
-		setTimeout(function(){
+		setTimeout(function() {
 			$(img).addClass('animated');
 		}, 1);
 	});
 
-	$('.gallery-img').bind('mouseleave', function(e){
+	$('.gallery-img').bind('mouseleave', function(e) {
 		$(this).removeClass(lastDirection).removeClass('animated');
 	});
 });
 
-var getDir = function( elem, e ) {
+var getDir = function(elem, e) {
 
 	// the width and height of the current div
 	var w = elem.width();
@@ -34,8 +36,8 @@ var getDir = function( elem, e ) {
 
 	// calculate the x and y to get an angle to the center of the div from that x and y.
 	// gets the x value relative to the center of the DIV and "normalize" it
-	var x = (e.pageX - offset.left - (w/2)) * ( w > h ? (h/w) : 1 );
-	var y = (e.pageY - offset.top  - (h/2)) * ( h > w ? (w/h) : 1 );
+	var x = (e.pageX - offset.left - (w / 2)) * (w > h ? (h / w) : 1);
+	var y = (e.pageY - offset.top - (h / 2)) * (h > w ? (w / h) : 1);
 
 	// the angle and the direction from where the mouse came in/went out clockwise (TRBL=0123);
 	/**
@@ -44,21 +46,17 @@ var getDir = function( elem, e ) {
 		divide by 90 to get the quadrant
 		add 3 and do a modulo by 4  to shift the quadrants to a proper clockwise TRBL (top/right/bottom/left)
 	**/
-	var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180 ) / 90 ) + 3 )  % 4;
+	var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4;
 
 	// do your animations here
-	switch(direction) {
+	switch (direction) {
 		case 0:
 			return 'top';
-		break;
 		case 1:
 			return 'right';
-		break;
 		case 2:
 			return 'bottom';
-		break;
 		case 3:
 			return 'left';
-		break;
 	}
-}
+};
--- js/script.singular.js
+++ js/script.singular.js
@@ -9,7 +9,7 @@
 /** jQuery jFontSize Plugin | Version 2.0 by Vincent Chabredier / Ouvrages | https://github.com/ouvrages/jfontsize **/
 // (function(a){return a.fn.jfontsize=function(e){var g,b,c,f,d;g=a(this);f={btnMinusClasseId:"#jfontsize-minus",btnDefaultClasseId:"#jfontsize-default",btnPlusClasseId:"#jfontsize-plus",btnMinusMaxHits:10,btnPlusMaxHits:10,sizeChange:1};if(e){e=a.extend(f,e)}d=function(){return a.jStorage.set("jfontsize",c)};b=function(){return g.each(function(j){var k,h;if(!(a(this).data("initial_size")!=null)){k=a(this).css("font-size");k=parseInt(k.replace("px",""));a(this).data("initial_size",k)}h=a(this).data("initial_size")+(c*e.sizeChange);return a(this).css("font-size",h+"px")})};a(e.btnMinusClasseId+", "+e.btnDefaultClasseId+", "+e.btnPlusClasseId).removeAttr("href");a(e.btnMinusClasseId+", "+e.btnDefaultClasseId+", "+e.btnPlusClasseId).css("cursor","pointer");c=a.jStorage.get("jfontsize",0);if(c===(-e.btnMinusMaxHits)){a(e.btnMinusClasseId).addClass("jfontsize-disabled")}if(c===e.btnPlusMaxHits){a(e.btnPlusClasseId).addClass("jfontsize-disabled")}b();a(e.btnMinusClasseId).click(function(){a(e.btnPlusClasseId).removeClass("jfontsize-disabled");if(c>(-e.btnMinusMaxHits)){c--;if(c===(-e.btnMinusMaxHits)){a(e.btnMinusClasseId).addClass("jfontsize-disabled")}b();return d()}});a(e.btnDefaultClasseId).click(function(){a(e.btnMinusClasseId).removeClass("jfontsize-disabled");a(e.btnPlusClasseId).removeClass("jfontsize-disabled");c=0;g.each(function(h){return a(this).css("font-size",a(this).data("initial_size")+"px")});return d()});return a(e.btnPlusClasseId).click(function(){a(e.btnMinusClasseId).removeClass("jfontsize-disabled");if(c<e.btnPlusMaxHits){c++;if(c===e.btnPlusMaxHits){a(e.btnPlusClasseId).addClass("jfontsize-disabled")}b();return d()}})}})(jQuery);
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
 	// $('textarea').autosize();
 	autosize($('textarea'));
@@ -27,13 +27,19 @@ jQuery(document).ready(function($){
 	// };
 
 	$('#text-justify, #text-unjustify').removeAttr('href').css('cursor', 'pointer');
-	$('#text-justify').click(function(e){e.preventDefault();
-		$('.entry-content p').each(function(){$(this).css('text-align', 'justify');});
+	$('#text-justify').click(function(e) {
+		e.preventDefault();
+		$('.entry-content p').each(function() {
+			$(this).css('text-align', 'justify');
+		});
 		$('#text-unjustify').fadeIn();
 		$('#text-justify').hide();
 	});
-	$('#text-unjustify').click(function(e){e.preventDefault();
-		$('body.rtl .entry-content p').each(function(){$(this).css('text-align', 'right');});
+	$('#text-unjustify').click(function(e) {
+		e.preventDefault();
+		$('body.rtl .entry-content p').each(function() {
+			$(this).css('text-align', 'right');
+		});
 		$('#text-justify').fadeIn();
 		$('#text-unjustify').hide();
 	});
