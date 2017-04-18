dbd3d04efe403d09f8cd04d27161aa8cd2b1bd76
Nasser Rafie <contact@geminorum.ir>
2017-04-18 05:22:53 +0430

    root: new template for full with page, supporting posts & pages

---
+++ fullwidthpage.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage gTheme 3
+ */
+/*
+Template Name: Full-Width Page
+Template Post Type: post, page
+*/
+
+get_template_part( 'singular', gtheme_template_base() );
