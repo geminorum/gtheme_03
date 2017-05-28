7153bc448c7bf0ac3e0ad25cf43043bbd566fd49
Nasser Rafie <contact@geminorum.ir>
2017-05-28 09:51:24 +0430

    module/attachment: :new: download helper

--- content-image.php
+++ content-image.php
@@ -3,6 +3,7 @@
 gThemeContent::wrapOpen( 'image' );
 
 	gThemeAttachment::image( array( 'tag' => 'big' ) );
+	gThemeAttachment::download();
 	gThemeAttachment::caption();
 	gThemeAttachment::backlink();
 
