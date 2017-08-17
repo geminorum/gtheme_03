704c85cecac51b7eb6383c4c5c93f104c86c7ef2
Nasser Rafie <contact@geminorum.ir>
2017-08-17 14:22:31 +0430

    root: new generic template parts

---
+++ sidebar-singular.php
@@ -0,0 +1,5 @@
+<?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+echo '<div class="col-sm-4 wrap-side sidebar-singular">';
+	gThemeSideBar::sidebar( 'side-singular' );
+echo '</div>';
