bea6d9a3d2ece6b0c148ba58583c667f290ca6dc
Nasser Rafie <contact@geminorum.ir>
2018-05-05 05:55:42 +0430

    root/start: moved navbar to partials

---
+++ partials/navbar-bs3.php
@@ -0,0 +1,15 @@
+<?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+echo '<nav ';
+	gThemeBootstrap::navbarClass( 'wrapper -navbar' );
+echo '><div class="container -nav">';
+
+	gThemeBootstrap::navbarHeader();
+
+	echo '<div id="navbar" class="navbar-collapse collapse yamm">';
+
+		// gThemeBootstrap::navbarForm( NULL, 'navbar-right' );
+		gThemeBootstrap::navbarNav( 'tertiary', FALSE, 'navbar-right' );
+		gThemeBootstrap::navbarNav( 'secondary', FALSE, 'navbar-left' );
+
+echo '</div></div></nav>';
