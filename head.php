7619d597abf1997e346938bab4a59777fba44745
Nasser Rafie <contact@geminorum.ir>
2018-05-05 05:52:04 +0430

    main/head: moved up charset meta

--- head.php
+++ head.php
@@ -1,11 +1,12 @@
 <?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
 
-?><!doctype html><?php
+?><!doctype html>
+<?php
 
 gThemeWrap::htmlOpen( '<head>' );
 
-?><meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
+?><meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta name="HandheldFriendly" content="true" /><?php
 
