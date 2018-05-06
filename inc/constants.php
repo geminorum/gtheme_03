a28d7c00aab77540949eee22001737c10c679de3
Nasser Rafie <contact@geminorum.ir>
2018-05-06 14:03:15 +0430

    root/systempage: specified by constant

--- inc/constants.php
+++ inc/constants.php
@@ -21,6 +21,7 @@ $gtheme_constants = array(
 	// 'GTHEME_WIDGET_THETERM_DISABLED' => FALSE, // will define by theme to skip the term info display
 	// 'GTHEME_SOCIAL_META_DISABLED' => FALSE, // will define by theme to skip social meta tags
 
+	// 'GTHEME_IS_SYSTEM_PAGE' => FALSE, // cannot set this early!
 	// 'GTHEME_IS_WP_SIGNUP'   => FALSE, // cannot set this early!
 	// 'GTHEME_IS_WP_ACTIVATE' => FALSE, // cannot set this early!
 
