a7fb657c72d6f39dc4bbe80a21740e26becac6e0
Nasser Rafie <contact@geminorum.ir>
2019-06-14 16:12:29 +0430

    root/content: partial for page entry only

--- content.php
+++ content.php
@@ -1,41 +1,49 @@
 <?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
 
-	if ( is_singular() || is_single() ) {
+if ( is_single() ) { // any posttype except attachment/page
 
-		gThemeContent::wrapOpen( 'singular' );
+	gThemeContent::wrapOpen( 'singular' );
 
-			gThemeImage::image( [
-				'tag'   => 'single',
-				'link'  => 'attachment',
-				'empty' => FALSE,
-			] );
+		gThemeImage::image( [
+			'tag'   => 'single',
+			'link'  => 'attachment',
+			'empty' => FALSE,
+		] );
 
-			if ( gThemeTerms::has( 'poster' ) ) {
+		if ( gThemeTerms::has( 'poster' ) ) {
 
-				// NO HEADER
-				// NO CONTENT
+			// NO HEADER
+			// NO CONTENT
 
-			} else {
-				get_template_part( 'partials/entry', 'singular' );
-			}
+		} else {
+			get_template_part( 'partials/entry', 'singular' );
+		}
 
-			gThemeSideBar::sidebar( 'after-singular' );
+		gThemeSideBar::sidebar( 'after-singular' );
 
-			gThemeComments::template( '<div class="entry-comments">', '</div>' );
+		gThemeComments::template( '<div class="entry-comments">', '</div>' );
 
-		gThemeContent::wrapClose( 'singular' );
+	gThemeContent::wrapClose( 'singular' );
 
-	} else {
+} else if ( is_page() ) {
 
-		gThemeContent::wrapOpen( 'index' );
+	gThemeContent::wrapOpen( 'page' );
 
-			gThemeImage::image( [
-				'tag'   => 'single',
-				'link'  => 'parent',
-				'empty' => FALSE,
-			] );
+		get_template_part( 'partials/entry', 'page' );
 
-			get_template_part( 'partials/entry', 'index' );
+	gThemeContent::wrapClose( 'page' );
 
-		gThemeContent::wrapClose( 'index' );
-	}
+} else {
+
+	gThemeContent::wrapOpen( 'index' );
+
+		gThemeImage::image( [
+			'tag'   => 'single',
+			'link'  => 'parent',
+			'empty' => FALSE,
+		] );
+
+		get_template_part( 'partials/entry', 'index' );
+
+	gThemeContent::wrapClose( 'index' );
+}
---
+++ partials/entry-page.php
@@ -0,0 +1,6 @@
+<?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+gThemeContent::header( [ 'context' => 'page', 'b1yline' => TRUE, 'a1ctions' => NULL ] );
+
+gThemeContent::content();
+// gThemeContent::navigation();
