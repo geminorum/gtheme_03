bbba84338b35b9e8bb4758a00f03f506ba565f07
Nasser Rafie <contact@geminorum.ir>
2019-06-14 16:39:22 +0430

    :wrench: maintenance

--- partials/entry-page.php
+++ partials/entry-page.php
@@ -1,6 +1,4 @@
 <?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
 
-gThemeContent::header( [ 'context' => 'page', 'b1yline' => TRUE, 'a1ctions' => NULL ] );
-
+gThemeContent::header( [ 'context' => 'page' ] );
 gThemeContent::content();
-// gThemeContent::navigation();
