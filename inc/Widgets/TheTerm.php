0d09b4df854f75904e450efba7c4c9b99a81a531
Nasser Rafie <contact@geminorum.ir>
2019-07-30 02:50:03 +0430

    widget/the term: before/after action hooks
    + term name as image alt
    + better html wrapping

--- inc/Widgets/TheTerm.php
+++ inc/Widgets/TheTerm.php
@@ -25,20 +25,36 @@ class gThemeWidgetTheTerm extends gThemeWidget
 		if ( ! $term = get_queried_object() )
 			return;
 
-		$desc  = get_term_field( 'description', $term->term_id, $term->taxonomy );
-		$image = ! empty( $instance['meta_image'] ) ? gThemeImage::termImage( [ 'term_id' => $term->term_id ] ) : FALSE;
+		$before = $after = $image = '';
 
-		if ( ! $desc && ! $image && ! empty( $instance['hide_no_desc'] ) )
+		$name = sanitize_term_field( 'name', $term->name, $term->term_id, $term->taxonomy, 'display' );
+		$desc = sanitize_term_field( 'description', $term->description, $term->term_id, $term->taxonomy, 'display' );
+
+		if ( ! empty( $instance['meta_image'] ) )
+			$image = gThemeImage::termImage( [ 'term_id' => $term->term_id, 'alt' => $name ] );
+
+		if ( ! empty( $instance['content_actions'] ) && has_action( 'gtheme_widget_the_term_before' ) ) {
+			ob_start();
+				do_action( 'gtheme_widget_the_term_before', $term, $instance, $name, $desc, $image );
+			$before = trim( ob_get_clean() );
+		}
+
+		if ( ! empty( $instance['content_actions'] ) && has_action( 'gtheme_widget_the_term_after' ) ) {
+			ob_start();
+				do_action( 'gtheme_widget_the_term_after', $term, $instance, $name, $desc, $image );
+			$after = trim( ob_get_clean() );
+		}
+
+		if ( ! $before && ! $after && ! $desc && ! $image && ! empty( $instance['hide_no_desc'] ) )
 			return;
 
 		$this->before_widget( $args, $instance );
-		$this->widget_title( $args, $instance, sanitize_term_field( 'name', $term->name, $term->term_id, $term->taxonomy, 'display' ) );
+		$this->widget_title( $args, $instance, $name );
 
-		if ( $image )
-			echo $image;
-
-		if ( $desc )
-			echo gThemeUtilities::prepDescription( $desc );
+		echo gThemeHTML::wrap( $image, 'gtheme-widget-image' );
+		echo gThemeHTML::wrap( $before, 'gtheme-widget-before' );
+		echo gThemeHTML::wrap( gThemeUtilities::prepDescription( $desc ), 'gtheme-widget-description' );
+		echo gThemeHTML::wrap( $after, 'gtheme-widget-after' );
 
 		$this->after_widget( $args, $instance );
 	}
@@ -51,8 +67,9 @@ class gThemeWidgetTheTerm extends gThemeWidget
 		$instance['title_link'] = strip_tags( $new['title_link'] );
 		$instance['class']      = strip_tags( $new['class'] );
 
-		$instance['meta_image']   = isset( $new['meta_image'] );
-		$instance['hide_no_desc'] = isset( $new['hide_no_desc'] );
+		$instance['meta_image']      = isset( $new['meta_image'] );
+		$instance['hide_no_desc']    = isset( $new['hide_no_desc'] );
+		$instance['content_actions'] = isset( $new['content_actions'] );
 
 		$this->flush_widget_cache();
 
@@ -69,6 +86,7 @@ class gThemeWidgetTheTerm extends gThemeWidget
 
 		$this->form_checkbox( $instance, TRUE, 'meta_image', _x( 'Display Meta Image', 'Widget: Setting', GTHEME_TEXTDOMAIN ) );
 		$this->form_checkbox( $instance, TRUE, 'hide_no_desc', _x( 'Hide if no Description', 'Widget: Setting', GTHEME_TEXTDOMAIN ) );
+		$this->form_checkbox( $instance, TRUE, 'content_actions', _x( 'Fire Before & After Actions', 'Widget: Setting', GTHEME_TEXTDOMAIN ) );
 
 		$this->after_form( $instance );
 	}
