b63655f89ffa33d745908d94e2ca5e323d7bad8f
Nasser Rafie <contact@geminorum.ir>
2019-08-10 17:19:31 +0430

    root/row: inline sub-template

---
+++ row-inline.php
@@ -0,0 +1,14 @@
+<?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+gThemeContent::header( [
+	'context'    => 'inline',
+	'prefix'     => 'row',
+	'meta'       => FALSE,
+	'amp'        => FALSE,
+	'anchor'     => FALSE,
+	'wrap_tag'   => FALSE,
+	'itemprop'   => FALSE,
+	'link_rel'   => FALSE,
+	'title_tag'  => 'h4',
+	'title_attr' => 'meta',
+] );
