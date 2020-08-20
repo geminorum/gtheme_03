6856cc20d5ed376b0741271bb93d5b7c0aa2180f
Nasser Rafie <contact@geminorum.ir>
2020-08-21 00:50:19 +0430

    partial/home: default dashboard with paginated carousel

--- partials/home.php
+++ partials/home.php
@@ -1,3 +1,4 @@
 <?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
 
+get_template_part( 'partials/dashboard', gtheme_template_base() );
 get_template_part( 'index', gtheme_template_base() );
