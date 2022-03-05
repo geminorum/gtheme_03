dc0860aa875dac9c4a2834fe976abcbf428835d9
Nasser Rafie <contact@geminorum.ir>
2022-03-06 00:27:53 +0330

    partial/band: :new: partial

--- start.php
+++ start.php
@@ -1,6 +1,7 @@
 <?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
 
 get_template_part( 'partials/navbar' );
+// get_template_part( 'partials/band', gtheme_template_base() );
 
 echo '<div class="wrapper -main">';
 echo '<div class="container container-wrap -main"><div class="row justify-content-center">';
