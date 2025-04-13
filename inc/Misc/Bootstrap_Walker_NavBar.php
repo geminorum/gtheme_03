f0338ee5a5cdd19c1257d66b27f0dd647985b0a7
Nasser Rafie <contact@geminorum.ir>
2025-04-13 16:22:23 +0330

    misc/bootstrap-nav-walker: click outside to close yamm

--- inc/Misc/Bootstrap_Walker_NavBar.php
+++ inc/Misc/Bootstrap_Walker_NavBar.php
@@ -41,7 +41,7 @@ class gThemeBootstrap_Walker_NavBar extends \Walker_Nav_Menu
 			$classes[] = 'yamm-fw';  // mega-menu
 
 			$output.= $indent.'<li class="'.gThemeHTML::prepClass( apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) ).'">';
-				$output.= '<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" data-bs-toggle="dropdown" data-display="static">'.esc_attr( $item->title ).'</a>';
+				$output.= '<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" data-bs-toggle="dropdown" data-display="static" data-bs-auto-close="outside">'.esc_attr( $item->title ).'</a>';
 					$output.= '<div class="dropdown-menu"><div class="yamm-content">';
 
 					ob_start();
