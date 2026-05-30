3debe29e3a637378f44ba002c6be4f916801ce58
Nasser Rafie <contact@geminorum.ir>
2026-05-30 20:35:37 +0330

    partial/navbar: migrate to bs v5.3

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,17 +1,11 @@
 <?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
 
-// echo '<nav class="wrapper -navbar"><div class="container-wrap -nav container">';
-gThemeWrap::wrapperOpen( 'navbar', FALSE, '-nav' );
+gThemeWrap::wrapperOpen( 'navbar', FALSE, '-nav', 'bg-primary-subtle' );
 
-	echo '<div class="-branding">';
-		gThemeTemplate::logo( 'navbar', '<h1><a class="navbar-brand no-outline" href="{{home_url}}" title="{{{logo_title}}}" rel="home">{{site_name}}</a></h1>' );
-		gThemeTemplate::about( '<small class="navbar-text">', '</small>' );
-	echo '</div>';
+	gThemeBootstrap::navbarOpen();
+	gThemeBootstrap::navbarNav( 'primary', 'navbar', 'yamm text-primary-emphasis' );
+	gThemeBootstrap::navbarWooCommerce( 'mx-2' );
+	gThemeBootstrap::navbarForm();
+	gThemeBootstrap::navbarClose();
 
-	echo '<div class="-rule"></div>';
-
-	gThemeMenu::nav( 'primary', [ 'class' => '-navigation -print-hide' ] );
-	// gThemeBootstrap::navbarNav( 'primary', FALSE, '-print-hide' );
-
-// echo '</div></nav>'."\n";
 gThemeWrap::wrapperClose( 'navbar', 2 );
