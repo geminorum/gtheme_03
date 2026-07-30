f3f9d56e17eaadec110dbdeb966f019699221855
Nasser Rafie <contact@geminorum.ir>
2026-07-30 20:34:56 +0330

    asset/css: content type header

--- css/index.php
+++ css/index.php
@@ -7,6 +7,8 @@ include '../../gtheme_03/assets/styles/loader-init.php';
 include '../../gtheme_03/assets/styles/loader-functions.php';
 include '../../gtheme_03/assets/styles/loader-headers.php';
 
+@header( 'Content-type: text/css; charset=utf-8' );
+
 ob_start( 'minify_css' );
 	include 'css.php';
 ob_end_flush();
