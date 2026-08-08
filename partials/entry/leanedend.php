e15c01899267496717c83edd446605eef824238d
Nasser Rafie <contact@geminorum.ir>
2026-08-08 12:48:09 +0330

    partial/entry: rethinking layout

---
+++ partials/entry/leanedend.php
@@ -0,0 +1,50 @@
+<?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+echo '<div class="entry-wrap-doublecolumn entry-wrap-double">';
+
+	echo '<div class="entry-double-top"><div class="-wrap">';
+		gThemeSideBar::sidebar( 'entry-before', '<div class="wrap-side sidebar-entry-before">', '</div>' );
+	echo '</div></div>';
+
+	// echo '<div class="-wrap -leaned-area-wrap -leaned-area-start">';
+	echo '<div class="-wrap -leaned-area-wrap -leaned-area-end">';
+		echo '<div class="-leaned-area entry-double-head"><div class="-wrap">';
+
+			gThemeContent::header( [
+				'context' => 'singular',
+				'byline'  => TRUE,
+				'actions' => NULL,
+			] );
+
+		echo '</div></div>';
+		echo '<div class="-leaned-area entry-double-image"><div class="-wrap">';
+
+			gThemeImage::image( [
+				'tag'   => 'single',
+				'link'  => 'attachment',
+				'empty' => FALSE,
+			] );
+
+		echo '</div></div>';
+		echo '<div class="-leaned-area -leaned-area-column entry-double-main"><div class="-wrap">';
+
+			gThemeContent::content();
+			gThemeSideBar::sidebar( 'entry-content', '<div class="wrap-side sidebar-entry-content">', '</div>' );
+
+		echo '</div></div>';
+		echo '<div class="-leaned-area -leaned-area-offset entry-double-side"><div class="-wrap">';
+
+			gThemeSideBar::sidebar( 'entry-side', '<div class="wrap-side sidebar-entry-side">', '</div>' );
+
+		echo '</div></div>';
+
+	echo '</div>'; // `.-leaned-area-wrap`
+
+	echo '<div class="entry-double-bottom"><div class="-wrap">';
+
+		gThemeSideBar::sidebar( 'entry-after', '<div class="wrap-side sidebar-entry-after">', '</div>' );
+		gThemeEditorial::tabsPostTabs();
+		gThemeNavigation::content( 'singular' );
+
+	echo '</div></div>';
+echo '</div>';
--- partials/entry/singlecolumn.php
+++ partials/entry/singlecolumn.php
@@ -2,64 +2,20 @@
 
 echo '<div class="entry-wrap-singlecolumn">';
 
-gThemeSideBar::sidebar( 'entry-before', '<div class="wrap-side sidebar-entry-before">', '</div>' );
+	gThemeSideBar::sidebar( 'entry-before', '<div class="wrap-side sidebar-entry-before">', '</div>' );
 
-gThemeImage::image( [
-	'tag'   => 'single',
-	'link'  => 'attachment',
-	'empty' => FALSE,
-] );
+	gThemeImage::image( [
+		'tag'   => 'single',
+		'link'  => 'attachment',
+		'empty' => FALSE,
+	] );
 
-gThemeContent::header( [ 'context' => 'singular', 'byline' => TRUE, 'actions' => NULL ] );
-gThemeEditorial::metaHTML( 'lead', [ 'before' => '<div class="entry-lead text-bg-light mb-5 pt-3 px-4 pb-1">', 'after' => '</div>' ] );
-
-if ( gThemeTerms::has( 'insert-people' ) )
-	gThemeEditorial::personPicture( [ 'before' => '<div class="entry-person">', 'after' => '</div>' ] );
-
-gThemeContent::content();
-// gThemeContent::navigation();
-gThemeContent::navigationFancy();
-
-gThemeEditorial::theAction( [
-	'before'     => '<div class="entry-after after-single after-action d-grid gap-2">',
-	'after'      => '</div>',
-	'link_class' => 'btn btn-lg btn-outline-primary',
-] );
-
-gThemeEditorial::theSource( [
-	'before' => '<div class="entry-after after-single after-source text-end">'.gThemeOptions::info( 'source_before', '' ),
-	'after'  => '</div>',
-] );
-
-gThemeEditorial::postLikeButton( [
-	'before' => '<div class="entry-after after-single after-like my-2">',
-	'after'  => '</div>',
-] );
-
-gThemeEditorial::refList( [
-	'before' => '<div class="entry-after after-single after-reflist my-2">',
-	'after'  => '</div>',
-	'title'  => gThemeOptions::info( 'reflist_title', FALSE ),
-	'wrap'   => FALSE,
-] );
-
-gThemeEditorial::addendumAppendages( [
-	'before' => '<div class="entry-after after-appendages after-rows my-2 -print-hide">',
-	'after'  => '</div>',
-	'wrap'   => FALSE,
-] );
-
-gThemeEditorial::venuePlace( [
-	'before' => '<div class="entry-after after-venue-place after-rows my-2">',
-	'after'  => '</div>',
-	'wrap'   => FALSE,
-	'title'  => sprintf( '<div class="-wrap-title"><h4 class="-title">%s</h4></div>', _x( 'Venue', 'Entry After Title', 'gtheme-ahmad' ) ),
-] );
-
-gThemeNavigation::content( 'singular', TRUE );
-
-gThemeEditorial::tabsPostTabs();
-
-gThemeSideBar::sidebar( 'entry-after', '<div class="wrap-side sidebar-entry-after">', '</div>' );
+	gThemeContent::header( [ 'context' => 'singular', 'byline' => TRUE, 'actions' => NULL ] );
+	gThemeContent::content();
+	gThemeSideBar::sidebar( 'entry-content', '<div class="wrap-side sidebar-entry-content">', '</div>' );
+	gThemeSideBar::sidebar( 'entry-side', '<div class="wrap-side sidebar-entry-side">', '</div>' );
+	gThemeSideBar::sidebar( 'entry-after', '<div class="wrap-side sidebar-entry-after">', '</div>' );
+	gThemeEditorial::tabsPostTabs();
+	gThemeNavigation::content( 'singular', TRUE );
 
 echo '</div>';
